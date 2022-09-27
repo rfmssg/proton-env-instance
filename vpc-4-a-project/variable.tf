7ac38a759876dcfba1103ab55c87e6afdfbefb12
sbx_user1051 <sbx_user1051@169.254.52.53>
2022-09-27 15:33:20 +0000

    Update requested for Proton - environment/vpc-4-a-project

---
+++ vpc-4-a-project/variable.tf
@@ -0,0 +1,12 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:eu-west-1:823983898304:environment/vpc-4-a-project
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "region" {
+  default = "eu-west-1"
+  type = string 
+}
